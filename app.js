1a0d22de31766c21e49243ed1591a9adef45a397
Tribbal <duarteweb@gmail.com>
2014-10-10 11:25:07 -0300

    Implementação de listas(laços) na veiw recuperadas do controller

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 (function(){
 var app = angular.module('minhaApp', [] ) ;
+
     app.controller('OlaCtrl', function( $scope ){
               $scope.nome = usuario.nome ;
               $scope.idade = usuario.idade ;
               $scope.email = usuario.email ;
-    }) ;
+    }) ;//end controller
+
+    app.controller('nacoesCtrl', function($scope){
+
+         $scope.nacoes = pais ;
+
+    }); //end controller
        
-       var usuario = {  nome: "Tião Macalé" ,
+    var pais = [
+                    { nome:"Brasil" }
+                  , { nome:"Argentina" }
+                  , { nome:"Espanha"}
+                  , { nome:"Tailandia"}
+                  
+               ] ;
+
+    var usuario = {  nome: "Tião Macalé" ,
                         idade: 368 ,
                         email: "tiaomaca@gmail.com"
                         }  
